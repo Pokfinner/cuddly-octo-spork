b1c49dc24929f6c5ef989020cf560722be2df7b4
Pokfinner <finkry@gmail.com>
2024-10-18 14:09:44 +0100

    adding diogo secret

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("Hello from prod")
+const app = require("express")()
+
+app.get("/", (req, res) => {
+    res.send("Server UP!")
+})
+
+app.listen(3005, () => console.log("Server running on port 3005"))
